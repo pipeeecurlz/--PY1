479c7e921750f8eadc2c0ce9eaddcd954d6a7556
pipeeecurlz <118249001+pipeeecurlz@users.noreply.github.com>
2022-11-21 21:24:40 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task6.py
@@ -0,0 +1,17 @@
+list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+list_max = 0
+list_max_index = 0
+
+for i, num in enumerate(list_numbers):
+    if num >= list_max:
+        list_max = num
+        list_max_index = i
+
+b = list_numbers[-1]
+list_numbers[list_max_index] = b
+list_numbers[-1] = list_max
+
+print(list_numbers)
+
+
