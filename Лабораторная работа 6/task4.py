e6415c4555eaba31b6b865c1fdbc03bf5592f50b
pipeeecurlz <118249001+pipeeecurlz@users.noreply.github.com>
2022-12-24 17:05:05 +0300

    Лабораторная работа 6

---
+++ Лабораторная работа 6/task4.py
@@ -0,0 +1,23 @@
+import json
+
+
+INPUT_FILE = "input.csv"
+
+
+def csv_to_list_dict(file_, delimiter=',', new_line='\n') -> list[dict]:
+    with open(file_) as f:
+        data = []
+        for index, line in enumerate(f):
+            lines = line.strip(new_line).split(delimiter)
+            if index == 0:
+                titles = lines
+                continue
+
+            data.append({})
+
+            for i, _ in enumerate(titles):
+                data[-1][titles[i]] = lines[i]
+    return data
+
+
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
