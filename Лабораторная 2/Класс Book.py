80be5988aeeaf2cc2d44323c4edfcdc5fa70ef1b
pipeeecurlz <118249001+pipeeecurlz@users.noreply.github.com>
2023-01-30 15:29:56 +0300

    Update Класс Book.py

--- Лабораторная 2/Класс Book.py
+++ Лабораторная 2/Класс Book.py
@@ -13,8 +13,8 @@ BOOKS_DATABASE = [
 
 
 class Book:
-    def __init__(self, id: int, name: str, pages: int):
-        self.id = id
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
         self.name = name
         self.pages = pages
 
@@ -22,13 +22,13 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self) -> str:
-        return f'Book(id_={self.id}, name={self.name!r}, pages={self.pages})'
+        return f'Book(id_={self.id_}, name={self.name!r}, pages={self.pages})'
 
 
 if __name__ == '__main__':
     # инициализируем список книг
     list_books = [
-        Book(id=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
     for book in list_books:
         print(book)  # проверяем метод __str__
